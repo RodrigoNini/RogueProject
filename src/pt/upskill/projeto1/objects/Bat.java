f9e02c37c8b0a1359bd90a856a836d9695048aa6
RodrigoNini <rodrigoninialves@gmail.com>
2023-07-27 19:19:15 +0100

    Movement updated on Adversarios

--- src/pt/upskill/projeto1/objects/Bat.java
+++ src/pt/upskill/projeto1/objects/Bat.java
@@ -1,6 +1,9 @@
 package pt.upskill.projeto1.objects;
 
 import pt.upskill.projeto1.rogue.utils.Position;
+import pt.upskill.projeto1.rogue.utils.Vector2D;
+
+import java.util.Random;
 
 public class Bat extends Adversarios{
 
@@ -8,6 +11,7 @@ public class Bat extends Adversarios{
     private int health;
     private int damage;
     private Position position;
+    Hero hero = Hero.getInstance();
 
     public Bat(Position position) {
         this.name = "Bat";
@@ -19,7 +23,6 @@ public class Bat extends Adversarios{
 
     @Override
     public void movement() {
-        Hero hero = Hero.getInstance();
         Position heroPosition = hero.getPosition();
         int heroX = heroPosition.getX();
         int heroY = heroPosition.getY();
@@ -35,6 +38,44 @@ public class Bat extends Adversarios{
 
             position = new Position(batX - 1, batY - 1);
         }
+    }
+
+    public void move() {
+        int distance = distanceBetween(this.position, hero.getPosition());
+        if (distance <= 3) {
+            moveNear();
+        } else {
+            moveRandom();
+        }
+    }
+
+    private void moveNear() {
+        Position heroPosition = hero.getPosition();
+        if (position.getX() < heroPosition.getX()) {
+            position = position.plus(new Vector2D(1, 0));
+        } else if (position.getX() > heroPosition.getX()) {
+            position = position.plus(new Vector2D(-1, 0));
+        }
+
+        if (position.getY() < heroPosition.getY()) {
+            position = position.plus(new Vector2D(0, 1));
+        } else if (position.getY() > heroPosition.getY()) {
+            position = position.plus(new Vector2D(0, -1));
+        }
+    }
+
+    private void moveRandom() {
+        //permite movimento na diagonal
+        Random random = new Random();
+        int dx = random.nextInt(3) - 1;
+        int dy = random.nextInt(3) - 1;
+        position = position.plus(new Vector2D(dx, dy));
+    }
+
+    private int distanceBetween(Position pos1, Position pos2) {
+        int distX = pos1.getX() - pos2.getX();
+        int distY = pos1.getY() - pos2.getY();
+        return distX + distY;
     }
 
     @Override
